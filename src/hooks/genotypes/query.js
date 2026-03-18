d1ab36191aeacf4839f7c2d88bfd72f8acadc009
hnolCol <h.nolte@uni-koeln.de>
2026-03-18 13:57:07 +0100

    fix_proteome_tag_genotype

--- src/hooks/genotypes/query.js
+++ src/hooks/genotypes/query.js
@@ -12,10 +12,11 @@ import config from "../../../config";
  * @returns {String[]} - The genotype tags matching the search string.
  */
 async function getGenotypesBySearchString_API({search_string, limit, proteome_tags}) {
-    const res = await axios.get(`${config.baseURL}/genotypes/q`, {params: {search_string, limit}})
+    const res = await axios.get(`${config.baseURL}/genotypes/q`, {params: {search_string, limit, proteome_tags}})
     return res.data
 }
 
-export const useGetGenotypesBySearchString = (APIParams = {search_string, limit, proteome_tags}, useQueryOptions = {}) => {
-    return useQuery(["getGenotypesBySearchString", APIParams.search_string, APIParams.limit, _.join(proteome_tags)],() =>  getGenotypesBySearchString_API({...APIParams}), useQueryOptions)
+export const useGetGenotypesBySearchString = (APIParams = { search_string, limit, proteome_tags }, useQueryOptions = {}) => {
+    const proteome_tag_string = _.isArray(proteome_tags) ? _.join(proteome_tags) : proteome_tags
+    return useQuery(["getGenotypesBySearchString", APIParams.search_string, APIParams.limit, proteome_tag_string],() =>  getGenotypesBySearchString_API({...APIParams}), useQueryOptions)
 }
