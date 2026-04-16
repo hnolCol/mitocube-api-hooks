fe479dad6d917d84861fd486ce25d40f9d5a6716
Parsa <parsa.ahmed35@gmail.com>
2026-04-16 13:55:17 +0200

    user hooks

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ import { useGetTermsOfUse } from "./src/hooks/info/terms";
 import { useGetFeatureInfo } from "./src/hooks/features/info";
 import { createSubmissionStateAPI } from "./src/hooks/submissions/state";
 import { useGetSubmissionSampleCount, useGetSubmissionSamplesFull, useGetSubmissionSampleTags } from "./src/hooks/submissions/samples";
-import { useGetUserRoleByTag, useGetUserRoles } from "./src/hooks/users/roles";
+import { createQueryUserRolesAPI } from "./src/hooks/users/roles";
 import { useGetSubmissionComments } from "./src/hooks/submissions/comments";
-import { useGetPublicUserByTag, usePatchUser, usePostUser } from "./src/hooks/users/users";
+import { createModifyUsersAPI } from "./src/hooks/users/users";
 import { createModifyTraitsAPI } from "./src/hooks/attributes/traits";
 import { useGetInstrument, useGetInstrumentsByType, useGetInstrumentState, useGetInstrumentStateByQuery, useGetInstrumentStateDurations, useGetInstrumentTypes, useGetSpecificInstrumentStateDurations, useGetStatesOfAnInstrument, usePostInstrumentState } from "./src/hooks/instruments/instruments";
 import { createModifySymptomsAPI } from "./src/hooks/maintenance/symptoms";
@@ -26,9 +26,9 @@ import { useGetMaintenanceEventStates } from "./src/hooks/maintenance/states";
 import { createModifyExternalServiceAPI  } from "./src/hooks/maintenance/externalservice"; 
 import { createQueryExternalServiceAPI } from "./src/hooks/maintenance/externalservice_query";
 import { useGetFeatureCorrelation } from "./src/hooks/features/correlation";
-import { useGetUserByQuery } from "./src/hooks/users/query_users";
-import { useGetUserCount } from "./src/hooks/users/counts";
-import { useGetUserIsActive } from "./src/hooks/users/active";
+import { createQueryUsersByQueryAPI } from "./src/hooks/users/query_users";
+import { createQueryUserCountAPI } from "./src/hooks/users/counts";
+import { createQueryUserActiveAPI } from "./src/hooks/users/active";
 import { useGetSubmissionConditionApplication, useGetSubmissionConditionApplicationAttributes, useGetSubmissionSampleConditionApplications, useGetSubmissionSampleConditionApplicationAttributes, useGetSubmissionConditionApplicationData, useUpdateSubmissionCA } from "./src/hooks/submissions/ca";
 import { createConditionApplicationAPI } from "./src/hooks/condition_applications/condition_applications";
 import { createSampleCoreAPI } from "./src/hooks/samples/samples";
@@ -74,6 +74,7 @@ import { createAuthenticationAPI } from "./src/hooks/authorization/login";
 import { createAuthenticationTokenAPI } from "./src/hooks/authorization/token";
 import { createSubmissionCoreAPI } from "./src/hooks/submissions/core";
 import { createStateAPI } from "./src/hooks/states/core";
+import { create } from "lodash";
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 
@@ -112,19 +113,16 @@ export default {
         createNewsAPI
     },
     users: {
-        useGetUserRoles,
-        useGetUserRoleByTag,
-        useGetPublicUserByTag,
-        useGetUserCount,
-        useGetUserIsActive,
-        usePostUser,
-        usePatchUser,
+        createModifyUsersAPI,
+        createQueryUserRolesAPI,
+        createQueryUserCountAPI,
+        createQueryUserActiveAPI,
         views: {
             createUserViewsAPI
         }
     },
     users_query: {
-        useGetUserByQuery
+        createQueryUsersByQueryAPI
     },
     genotypes: {
         createModifyGenotypesAPI,
--- src/hooks/users/query_users.js
+++ src/hooks/users/query_users.js
@@ -1,22 +1,28 @@
 import { useQuery } from "@tanstack/react-query"
-import axios from "axios"
 
-/**
- * @description Returns the public information about the users. Still requires a valid token string. Public indicates here that it is available to all registered users. 
- * @param {Object} props 
- * @param {String} props.query 
- * @returns {String[]} The user tag.
- */
-async function getPublicUsersByQuery_API({ search_string, limit }) {
-    const res = await axios.get('/api/users/q', {params : { search_string, limit }})
-    return res.data
+export function createQueryUsersByQueryAPI(client) {
+
+    /**
+     * @description Returns the public information about the users. Still requires a valid token string. Public indicates here that it is available to all registered users. 
+     * @param {Object} props 
+     * @param {String} props.query 
+     * @returns {String[]} The user tag.
+     */
+    async function getPublicUsersByQuery_API({ search_string, limit }) {
+        const res = await client.get('/users/q', {params : { search_string, limit }})
+        return res.data
+    }
+
+    const useGetUserByQuery = (APIParams = {search_string, limit}, useQueryOptions = {staleTime: 500000, placeholderData: (prev) => prev ? prev : []}) => {
+        return useQuery({
+            queryKey: ["getUserQuery", APIParams.search_string, APIParams.limit],
+            queryFn: () => getPublicUsersByQuery_API({...APIParams}),
+            ...useQueryOptions
+        })
+    }
+
+    return {
+        useGetUserByQuery
+    }
+
 }
-
-export const useGetUserByQuery = (APIParams = {search_string, limit}, useQueryOptions = {staleTime: 500000, placeholderData: (prev) => prev ? prev : []}) => {
-    return useQuery({
-        queryKey: ["getUserQuery", APIParams.search_string, APIParams.limit],
-        queryFn: () => getPublicUsersByQuery_API({...APIParams}),
-        ...useQueryOptions
-    })
-}
-
